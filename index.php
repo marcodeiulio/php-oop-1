0f172d8dd8386fc44bdfa4448a205255b842a19c
marcodeiulio <mdeiulio@icloud.com>
2022-03-02 16:54:39 +0100

    - Added class Movie - Added instances - Added __construct

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+class Movie
+{
+	public $title;
+	public $year;
+	public $director;
+
+	function __construct($title, $year, $director)
+	{
+		$this->title = $title;
+		$this->year = $year;
+		$this->director = $director;
+	}
+}
+
+$django_unchained = new Movie('Django Unchained', '2013', 'Quentin Tarantino');
+$green_mile = new Movie('The Green Mile', '1999', 'Frank Darabont');
+$titanic = new Movie('Titanic', '1998', 'James Cameron');
+$shutter_island = new Movie('Shutter Island', '2010', 'Martin Scorsese');
+
+var_dump($django_unchained);
+var_dump($green_mile);
+var_dump($titanic);
+var_dump($shutter_island);
